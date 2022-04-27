b736f79bb9f9db438dec52b3abee3e21fde5e4e8
hpstory <33348162+hpstory@users.noreply.github.com>
2022-04-27 15:47:54 +0800

    remove duplicate codes

--- IdentityDemo/IdentityDemo/Program.cs
+++ IdentityDemo/IdentityDemo/Program.cs
@@ -13,7 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DemoDbContext>(options =>
 {
     options.UseSqlServer(
