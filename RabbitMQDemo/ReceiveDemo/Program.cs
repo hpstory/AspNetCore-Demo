a2fa1fab2ea5315c3bce095d8e04854aea838efb
hpstory <354195783@qq.com>
2022-04-28 16:43:10 +0800

    rabbitmq demo

---
+++ RabbitMQDemo/ReceiveDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.DispatchConsumersAsync = true;
+string exchangeName = "demoExchange";
+string eventName = "demoEvent";
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+string queueName = "demoQueue1";
+channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+channel.QueueDeclare(
+    queue: queueName,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+channel.QueueBind(
+    queue: queueName, exchange: exchangeName, routingKey: eventName);
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+Console.ReadLine();
+
+async Task Consumer_Received(object sender, BasicDeliverEventArgs args)
+{
+    try
+    {
+        var bytes = args.Body.ToArray();
+        string message = Encoding.UTF8.GetString(bytes);
+        Console.WriteLine("received: " + message);
+        channel.BasicAck(args.DeliveryTag, multiple: false);
+        await Task.Delay(800);
+    }
+    catch (Exception e)
+    {
+        channel.BasicReject(args.DeliveryTag, true);
+        Console.WriteLine("error: " + e);
+    }
+}
---
+++ RabbitMQDemo/SendDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
+connectionFactory.DispatchConsumersAsync = true;
+string exchangeName = "demoExchange";
+string exchangeType = "direct";
+var connection = connectionFactory.CreateConnection();
+while (true)
+{
+    using var channel = connection.CreateModel();
+    var property = channel.CreateBasicProperties();
+    property.DeliveryMode = 2;
+    channel.ExchangeDeclare(exchangeName, exchangeType);
+    byte[] body = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+    channel.BasicPublish(
+        exchange: exchangeName,
+        routingKey: "demoEvent",
+        mandatory: true,
+        basicProperties: property,
+        body: body);
+    Console.WriteLine("send");
+    Thread.Sleep(5000);
+}
+
